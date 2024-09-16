03e04eed78b81cf356d1a690342b29bd7be0e08c
GTXtab <malahovskinazar007@gmail.com>
2024-09-16 22:55:32 +0300

    add Windows.h

--- Lab_3.1/Lab_3.1.cpp
+++ Lab_3.1/Lab_3.1.cpp
@@ -6,11 +6,15 @@
 
 #include <iostream>
 #include <cmath>
+#include <Windows.h>
 
 using namespace std;
 
 int main()
 {
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
 	double x;
 	double y;
 
